4cbf04b5e6c482ad00cc22ae781f0a26b7e12b00
Arya Massarat <aryam7@gmail.com>
2020-02-13 07:55:00 -0800

    restructure import_labelme script for better importing

--- scripts/import_labelme.py
+++ scripts/import_labelme.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 import json
 
-def main(data):
-    with open(data) as json_file:
-        data = json.load(json_file)['shapes']
-        for i in range(len(data)):
-            data[i] = data[i]['points']
-        return data
-
 if __name__ == '__main__':
+    # if this script is being called but not imported:
     import argparse
-    parser = argparse.ArgumentParser(description='Import the labelme data.')
+    parser = argparse.ArgumentParser(description='Import the labelme labels as a simple python list.')
     parser.add_argument(
-        "data", nargs="?",
-        help="a path to the json file containing the labelme labels"
+        "labels", help="the path to the json file containing the labelme labels"
     )
     args = parser.parse_args()
-    print(main(args.data))
+    labels = args.labels
+elif labels is None:
+    # otherwise, if it is being imported and the labels var isn't set
+    raise Exception("you must set the labels var to the path to the json file containing the labelme labels before importing this module")
+
+# import the labels and extract the coordinates to a list
+with open(labels) as json_file:
+    labels = json.load(json_file)['shapes']
+    for i in range(len(labels)):
+        labels[i] = labels[i]['points']
+
+# if this script is being called but not imported, print the data to stdout
+if __name__ == '__main__':
+    print(labels)
