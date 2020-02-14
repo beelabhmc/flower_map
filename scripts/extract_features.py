542ef1ed9074685f0a515f5fce84e49f0025c271
Arya Massarat <aryam7@gmail.com>
2020-02-14 10:16:05 -0800

    pass mask to each feature function

--- scripts/extract_features.py
+++ scripts/extract_features.py
@@ -19,10 +19,9 @@ args = parser.parse_args()
 
 # if the data is from labelme, import it using the labelme importer
 if args.labels.endswith('.json'):
-    labels = args.labels
     import import_labelme
     # labels = [np.array(label, dtype=np.int32) for label in labels]
-    labels = [tuple(label) for label in labels]
+    labels = import_labelme.main(args.labels)
 else:
     raise Exception('label format not supported yet')
 
@@ -34,18 +33,19 @@ img_array = np.asarray(img)
 # initialize a np array to store the output data
 out = np.empty((len(labels), 19))
 
-def metrics(region):
+def metrics(img, mask):
     """
-        input: region - pixels extracted from each image which correspond with a segmented region
+        input: img - an image from which to grab the pixels in the segmented region given by the boolean mask
+               mask - a boolean mask with true values at pixel values contained within the segmented region
         output: metrics - all of the features we can calculate for that segmented region
     """
-    avg = features.colorAvg(region)
-    yellow = features.yellowFast(region)
-    edges = features.countEdgePixels(region)
-    var = features.colorVariance(region)
-    texture = features.textureAnalysis(region)
-    (contrast, dissim, homog, energy, corr, ASM) = features.glcm(region)
-    (Hstd, Sstd, Vstd, Hskew, Sskew, Vskew) = features.colorMoment(region)
+    avg = features.colorAvg(img, mask)
+    yellow = features.yellowFast(img, mask)
+    edges = features.countEdgePixels(img, mask)
+    var = features.colorVariance(img, mask)
+    texture = features.textureAnalysis(img, mask)
+    (contrast, dissim, homog, energy, corr, ASM) = features.glcm(img, mask)
+    (Hstd, Sstd, Vstd, Hskew, Sskew, Vskew) = features.colorMoment(img, mask)
     metrics = [avg[0], avg[1], avg[2], yellow, var, edges, texture, contrast, dissim, homog, energy, corr, ASM, Hstd, Sstd, Vstd, Hskew, Sskew, Vskew]
     return metrics
 
@@ -55,17 +55,24 @@ for i in range(len(labels)):
     # calculate a boolean mask of the region contained within the provided contour
     # mask = np.zeros((img.shape[0], img.shape[1])
     mask = Image.new('L', (img_array.shape[1], img_array.shape[0]), 0)
-    ImageDraw.Draw(mask).polygon(labels[0], outline=1, fill=1)
-    # mask = np.array(mask)
+    ImageDraw.Draw(mask).polygon(
+        [tuple(label) for label in labels[i]],
+        outline=1, fill=1
+    )
+    mask = np.array(mask)
     # use the boolean mask to extract only those pixels of the img
     # assemble new image (uint8: 0-255)
     new_img_array = np.empty(img_array.shape,dtype='uint8')
     # colors (three first columns, RGB)
     new_img_array[:,:,:3] = img_array[:,:,:3]
+    # filtering image by mask
+    new_img_array[:,:,0] = new_img_array[:,:,0] * mask
+    new_img_array[:,:,1] = new_img_array[:,:,1] * mask
+    new_img_array[:,:,2] = new_img_array[:,:,2] * mask
     new_img = Image.fromarray(new_img_array, "RGB")
 
     # calculate the features and store them in the np array
-    out[i,:] = metrics(new_img)
+    out[i,:] = metrics(new_img, mask)
 
 # write the output to the tsv file
 np.savetxt(args.out, out, fmt='%f', delimiter="\t")
