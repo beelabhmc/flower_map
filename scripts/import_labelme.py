1c42bc7f2b13be83d5ccfeedbe73bf0a3a18f25f
Arya Massarat <aryam7@gmail.com>
2020-02-14 10:15:31 -0800

    fix import labelme script

--- scripts/import_labelme.py
+++ scripts/import_labelme.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 import json
 
+
+def main(labels):
+    # import the labels and extract the coordinates to a list
+    with open(labels) as json_file:
+        labels = json.load(json_file)['shapes']
+        for i in range(len(labels)):
+            labels[i] = labels[i]['points']
+    return labels
+
 if __name__ == '__main__':
     # if this script is being called but not imported:
     import argparse
@@ -9,17 +18,5 @@ if __name__ == '__main__':
         "labels", help="the path to the json file containing the labelme labels"
     )
     args = parser.parse_args()
-    labels = args.labels
-elif labels is None:
-    # otherwise, if it is being imported and the labels var isn't set
-    raise Exception("you must set the labels var to the path to the json file containing the labelme labels before importing this module")
-
-# import the labels and extract the coordinates to a list
-with open(labels) as json_file:
-    labels = json.load(json_file)['shapes']
-    for i in range(len(labels)):
-        labels[i] = labels[i]['points']
-
-# if this script is being called but not imported, print the data to stdout
-if __name__ == '__main__':
+    main(args.labels)
     print(labels)
