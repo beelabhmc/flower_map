6cd8859f0ebe20adca9be2843870faa9f7dc9085
Arya Massarat <aryam7@gmail.com>
2020-04-05 20:30:50 -0700

    move utility functions for testing to a different script

--- scripts/segment.py
+++ scripts/segment.py
@@ -15,30 +15,8 @@ args = parser.parse_args()
 import features
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
+from test_util import *
 
-
-def plot_img(imgs, titles=None):
-    fig, axes = plt.subplots(*imgs.shape, figsize=(15.3,7.4))
-    for i, ax in np.ndenumerate(axes):
-        if len(i) == 1:
-            i = (0,) + i
-        ax.imshow(imgs[i])
-        if titles is not None:
-            ax.set_title(titles[i])
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-    fig.tight_layout()
-    print('showing plot')
-    fig.show()
-    exit()
-
-def create_arr(lst, *shape):
-    if not shape:
-        shape = (1, len(lst))
-    arr = np.empty(len(lst), dtype=object)
-    arr[:] = lst[:]
-    return arr.reshape(shape)
 
 def sliding_window(img, size, fnctn, num_features=1, skip=0):
     """
---
+++ scripts/test_util.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def plot_img(imgs, titles=None):
+    fig, axes = plt.subplots(*imgs.shape, figsize=(15.3,7.4))
+    for i, ax in np.ndenumerate(axes):
+        if len(i) == 1:
+            i = (0,) + i
+        ax.imshow(imgs[i])
+        if titles is not None:
+            ax.set_title(titles[i])
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+    fig.tight_layout()
+    print('showing plot')
+    fig.show()
+    exit()
+
+def create_arr(lst, *shape):
+    if not shape:
+        shape = (1, len(lst))
+    arr = np.empty(len(lst), dtype=object)
+    arr[:] = lst[:]
+    return arr.reshape(shape)
