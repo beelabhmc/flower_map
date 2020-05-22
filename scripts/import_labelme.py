beelabhmc/flower_map

commit 2f4c4e3642f1575ce72739cdfec1b5a800e70589

make sure labelme file paths are relative to the location of the file

scripts/import_labelme.py
@@ -40,6 +40,8 @@ def write(file, segments, image_path=None):
         segments can be either a list of tuples (label, pts) or a simple list of pts
         if image_path is provided, the file will be in valid labelme format
     """
+    import os.path
+    image_path = os.path.relpath(image_path, file)
     with open(file, 'w') as out:
         json.dump(
             {

commit 2544d50e9ef8b4140d35f8e85bcee8dc989d4ac9

fix bug introduced in import_labelme from 2f4c4e3

scripts/import_labelme.py
@@ -40,8 +40,9 @@ def write(file, segments, image_path=None):
         segments can be either a list of tuples (label, pts) or a simple list of pts
         if image_path is provided, the file will be in valid labelme format
     """
-    import os.path
-    image_path = os.path.relpath(image_path, file)
+    if image_path:
+        import os.path
+        image_path = os.path.relpath(image_path, file)
     with open(file, 'w') as out:
         json.dump(
             {