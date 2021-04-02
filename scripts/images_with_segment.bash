80a2733437891ef4a1abdf176ebd01f4c642eb63
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-04-02 10:29:00 -0700

    allow for trailing slashes in the directory param

--- scripts/images_with_segment.bash
+++ scripts/images_with_segment.bash
@@ -4,14 +4,14 @@
 # arg1: path to the directory of the region in the output folder (ex: out/6617East1)
 # arg2: a segment ID (ex: 339)
 
-# ex usage: ./images_with_segment.bash out/6617East1 339
+# ex usage: scripts/images_with_segment.bash out/6617East1 339
 # ex output:
 # DJI_0283
 # DJI_0284
 
-if [ ! -d "$1"/rev_transforms ]; then
-  echo "The directory $1/rev_transforms/ must exist in order to run this script. Check that you are using the experimental strategy of the pipeline and that the 'parallel' config option is set to true."
+if [ ! -d "${1%/}"/rev_transforms ]; then
+  echo "The directory ${1%/}/rev_transforms/ must exist in order to run this script. Check that you are using the experimental strategy of the pipeline and that the 'parallel' config option is set to true."
   exit 1
 fi
-cd "$1"/rev_transforms
+cd "${1%/}"/rev_transforms
 grep -cl '"label": "'$2'"' *.json | sed 's/.json//g'
